1f93f0204057e13cac313fe873aa06c556919511
imos <github@imoz.jp>
2014-08-17 02:03:10 +0900

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-docker-sslh
-===========
+sslh in Docker
+==============
 
-sslh in Docker.
+Install sslh inside Docker.
+
+How to Install
+--------------
+
+Docker must be installed beforehand.
+
+How to Run sslh
+---------------
+
+Run the following command:
+
+```sh
+make start
+```
---
+++ service.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+: ${SERVICE:=sslh}
+: ${CPU:=100}
+: ${MEMORY:=100M}
+: ${DISK:=5G}
+
+set -e -u
+
+if [ "$(whoami)" != 'root']; then
+  echo 'this script must run as root.' >&2
+  exit 1
+fi
+
+docker_build() {
+  docker build --tag="${SERVICE}" .
+}
+
+docker_run() {
+  local docker_flags=("${DOCKER_FLAGS[@]}")
+  docker_flags+=(--memory="${MEMORY}" --cpu-shares="${CPU}")
+  docker run "${docker_flags[@]}" "${SERVICE}" "$@"
+}
+
+start() {
+  if docker top "${SERVICE}" >/dev/null 2>/dev/null; then
+    echo 'docker is already running.' >&2
+  fi
+  DOCKER_FLAGS=(--name="${SERVICE}" --hostname="${SERVICE}" --detach)
+  docker_build
+  docker_run "$@"
+}
+
+stop() {
+  docker kill "${SERVICE}" >/dev/null || true
+  docker rm "${SERVICE}" >/dev/null || true
+}
+
+install() {
+  docker_build
+}
+
+command="$1"
+shift
+case "${command}" in
+  'start') start "$@";;
+  'stop') stop;;
+  'install') install;;
+  'uninstall') :;;
+  *) echo "no such command: ${command}" >&2;;
+esac
